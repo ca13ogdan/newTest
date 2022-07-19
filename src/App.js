e0b224dffe2be9d100a1db29970fb6be98d2376d
Covrig Bogdan <ca13ogdan@gmail.com>
2022-07-19 11:39:16 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Chart from "react-apexcharts";
 import './App.css';
+import {
+  Card,
+  Dimmer,
+  Loader,
+  Select
+} from 'semantic-ui-react';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [priceData, setPriceData] = useState(null);
+  const [currency, setCurrency] = useState(null);
+  const [chartData, setChartData] = useState(null);
+  const [series, setSeries] = useState(null);
+
+  const options = [
+    { value: 'USD', text: 'USD' }
+
+  ];
+
+  useEffect(() => {
+    async function fetchPrices() {
+      const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+      const data = await res.json();
+      setCurrency(data.bpi.USD.code);
+      setPriceData(data.bpi);
+      getChartData();
+    }
+    fetchPrices();
+  }, []);
+
+  const getChartData = async () => {
+    const res = await fetch(`https://api.coindesk.com/v1/bpi/historical/close.json?`)
+    const data = await res.json();
+    const categories = Object.keys(data.bpi);
+    const series = Object.values(data.bpi);
+    setChartData({
+      xaxis: {
+        categories: categories
+      }
+    })
+    setSeries([
+      {
+        name: "Bitcoin Price",
+        data: series
+      }
+    ])
+    setLoading(false);
+  }
+
+  const handleSelect = (e, data) => {
+    setCurrency(data.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className="nav" style={{ padding: '15px', backgroundColor: 'gold' }}>
+        Coindesk API Data
+      </div>
+      {loading ? (
+        <div>
+          <Dimmer active inverted>
+            <Loader>Loading</Loader>
+          </Dimmer>
+        </div>
+      ) : (
+        <>
+          <div className="price-container"
+            style={{
+              display: 'flex', justifyContent: 'space-around', alignItems: 'center',
+              width: 600,
+              height: 300,
+              margin: '0 auto'
+            }}>
+
+            <div className='price'>
+              <Card>
+                <Card.Content>
+                  <Card.Header> Portofolio Balance</Card.Header>
+                  <Card.Description>$ {priceData[currency].rate}</Card.Description>
+                </Card.Content>
+              </Card>
+            </div>
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <Chart
+              options={chartData}
+              series={series}
+              type="line"
+              width="1200"
+              height="300"
+            />
+          </div>
+        </>
+      )}
     </div>
   );
 }
